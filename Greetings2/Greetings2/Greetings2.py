4e37b81eccdb80d3ee4430b44544cf61b3cb227d
tvangeest <tommygeest@hotmail.com>
2015-09-15 16:17:00 +0200

    First

---
+++ Greetings2/Greetings2/Greetings2.py
@@ -0,0 +1,17 @@
+﻿print ("Hello User, I am Python")
+name = raw_input("What is your name?: ")
+print "Hello", name
+print ("_________")
+print ("|xxxxxxx|")
+print ("|xxxxxx |")
+print ("|xxxxx  |")
+print ("|xxxx   |")
+print ("|xxx    |")
+print ("|xx     |")
+print ("|x      |")
+print ("---------")
+
+x = 0
+while x != 10
+print ("*" * x)
+x +=1
