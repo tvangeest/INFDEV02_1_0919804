6489b582797bb4ee5d638aa50ee69c88a1a9e046
tvangeest <tommygeest@hotmail.com>
2015-10-04 04:05:40 +0200

    Assignment 6 square and triangle

--- Assigment6/Assigment6/Assigment6.py
+++ Assigment6/Assigment6/Assigment6.py
@@ -1,13 +1,23 @@
 print "Make some figures!"
 x = "*"
 space = " "
+w = 1
 figure = raw_input("square, triangle, circle or smiley?: ").lower()
-amount = raw_input("How long do you want your figure(width): ")
-line = raw_input("How many lines do you want your figure(height): ")
 if figure == "square":
-    print "[]"
+    width = int(raw_input("How long do you want your figure(width): "))
+    height = int(raw_input("How many lines do you want your figure(height): "))
+    while w != width:
+        x = x + "*"
+        w = w + 1
+    for z in range(0, height):
+        print x
+        z = z + 1
 elif figure == "triangle":
-    print "triangle"
+    height = int(raw_input("How many lines do you want your figure(height): "))
+    for z in range(0, height):
+        print x
+        x = x + "*"
+        z = z + 1
 elif figure == "circle":
     print "( )"
 elif figure == "smiley":
