efc1aa8fdb56c2c6dc7f9bb7b81c4f96c63ae13e
tvangeest <tommygeest@hotmail.com>
2015-10-05 17:07:34 +0200

    changes

--- Assignment5Cryptography/Assignment5Cryptography/Assignment5Cryptography.py
+++ Assignment5Cryptography/Assignment5Cryptography/Assignment5Cryptography.py
@@ -1,1 +1,11 @@
-﻿
+﻿import sys
+word = raw_input("Input your letters: ")
+n = int(raw_input("Input the amount of letters you want to shift: "))
+z = 0
+
+for c in word:
+    c = ord(c)
+    c = c + n
+    c = chr(c)
+    sys.stdout.write(c) # print without enter
+print "" # print empty line
--- Assignment5PasswordCheck/Assignment5PasswordCheck/Assignment5PasswordCheck.py
+++ Assignment5PasswordCheck/Assignment5PasswordCheck/Assignment5PasswordCheck.py
@@ -1,10 +1,11 @@
-def passwordCheck():
+﻿repeat = "yes"
+while repeat == "yes":
     password = raw_input("Please input a password: ")
     if len(password) <= 4:
         print "This password is too weak, please try again."
-        passwordCheck()
+        repeat = raw_input("Try again?: ")
     elif len(password) >= 10:
         print "This password is very strong!"
     elif password.isupper or password.isspace or password.isdigit:
         print "This password is medium"
-passwordCheck()
+    repeat = raw_input("Try again?")
