03a9ab05ae0e3fea39e26c496d9041345d0a41a6
tvangeest <tommygeest@hotmail.com>
2015-10-07 01:25:48 +0200

    all assignments done

--- Assignment5PasswordCheck/Assignment5PasswordCheck/Assignment5PasswordCheck.py
+++ Assignment5PasswordCheck/Assignment5PasswordCheck/Assignment5PasswordCheck.py
@@ -1,13 +1,17 @@
 ﻿repeat = "yes"
 while repeat == "yes":
     password = raw_input("Please input a password: ")
+    count = 0
+    for letter in password:
+        if letter.isupper() or letter.isspace() or letter.isdigit():
+            count += 1
     if len(password) <= 4:
         print "This password is too weak, please try again."
-        repeat = raw_input("Try again?: ")
-    elif len(password) >= 15:
+    elif len(password) >= 14:
         print "This password is very strong!"
-    elif password.isupper() or password.isdigit():
+        repeat = "no"
+    elif count >= 6:
         print "This password is very strong!"
+        repeat = "no"
     else:
-        print "This password is medium strength!"
-    repeat = raw_input("Try again?: ")
+        print "This password is medium strength, please try again."
