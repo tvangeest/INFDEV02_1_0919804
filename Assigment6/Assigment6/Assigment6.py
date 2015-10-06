f9ff5328c9510f9a4d08679be69c339497b1d76f
tvangeest <tommygeest@hotmail.com>
2015-10-06 15:47:21 +0200

    changes

--- Assigment6/Assigment6/Assigment6.py
+++ Assigment6/Assigment6/Assigment6.py
@@ -4,7 +4,7 @@ x = ""
 full = ""
 emptyLine = ""
 emptyRows = ""
-space = " "
+space = ""
 finished = ""
 figure = raw_input("square, triangle, circle or smiley?: ").lower()
 if figure == "square":
@@ -22,7 +22,7 @@ if figure == "square":
         x = x + "*"
         for i in range(0, width):
             full = full + "*"
-        for i in range(1, (width-2)):
+        for i in range(1, (width-1)):
             space = space + " "
         emptyLine = ( emptyLine + x + space + x + "\n")
         for i in range(1, (width-1)):
@@ -39,7 +39,15 @@ elif figure == "triangle":
         sys.stdout.write(finished)
     elif triangleType == "2":
         height = int(raw_input("How many lines do you want your figure(height): "))
-elif figure == "circle":
-    print "( )"
+        x = x + "*"
+        space = space + " "
+        for i in range(0,height):
+            sys.stdout.write("\n")
+            for j in range(0,i):
+                for k in range(0, height-j):
+                    sys.stdout.write(space)
+                sys.stdout.write(x)
+    elif figure == "circle":
+        print "( )"
 elif figure == "smiley":
     print ":)"
---
+++ Assignment6Figures/Assignment6Figures/Assignment6Figures.py
@@ -0,0 +1,33 @@
+﻿import sys
+done = ""
+figure = raw_input("square1, square2, triangle1, triangle2: ").lower()
+if figure == "square1":
+    length = int(raw_input("What is the length of the square?: "))
+    width = int(raw_input("What is the width of the square?: "))
+    for i in range(width):
+        for j in range(length):
+            done += "*"
+        done += "\n"
+    sys.stdout.write(done)
+elif figure == "square2":
+    length = int(raw_input("What is the length of the square?: "))
+    width = length
+    for i in range(width):
+        done += "*"
+    done += "\n"
+    for j in range(width-2):
+        done += "*"
+        for k in range(width-2):    
+            done += " "
+        done += "*\n"
+    for l in range(width):
+        done += "*"
+    sys.stdout.write(done + "\n")
+elif figure == "triangle1":
+    length = int(raw_input("What is the length of the triangle?"))
+    for i in range(length):
+        for j in range(i+1):
+            done += "*"
+        done += "\n"
+    sys.stdout.write(done)
+
